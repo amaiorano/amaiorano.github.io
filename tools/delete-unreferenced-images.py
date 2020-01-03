de8ed1d12a48ca4925c7875691c09ca70235e807
Antonio Maiorano <amaiorano@gmail.com>
2020-01-02 22:57:38 -0500

    Improved delete-unreferenced-images.py
    * Better path resolution using Path.resolve to match casing
    * Works from any directory
    * Raises an exception if unexpected number of files are unreferenced

--- tools/delete-unreferenced-images.py
+++ tools/delete-unreferenced-images.py
@@ -1,4 +1,5 @@
 # Delete unreferenced images
+# Usage: delete-unreferenced-images.py <path_to_post> <path_to_images_root>
 
 import sys
 import os
@@ -6,15 +7,17 @@ import re
 import glob
 import tempfile
 import shutil
+from pathlib import Path
 from os import path
 
 post = sys.argv[1]
 img_dir = sys.argv[2]
 
-# chdir to root path
+def abs_resolve_path(p):
+    return str(Path(path.abspath(p)).resolve())
+
 script_path = path.dirname(os.path.realpath(__file__))
 root_path = path.abspath(path.join(script_path, '..'))
-print("chdir to: " + root_path)
 
 # Find files referenced by post
 post_files = set()
@@ -22,13 +25,13 @@ for line in open(post):
     m = re.match(r'\!\[\]\((.*)\)', line)
     if m:
         img_file = m.groups()[0]
-        post_files.add(path.abspath('.' + path.sep + img_file))
+        post_files.add(abs_resolve_path(root_path + path.sep + img_file))
 
 # Find all files in img_dir
-all_files = set([path.abspath(f) for f in glob.glob(img_dir + path.sep + "*.*", recursive=False)])
+all_files = set([abs_resolve_path(f) for f in glob.glob(img_dir + path.sep + "*.*", recursive=False)])
 
-# print(post_files)
-# print(all_files)
+# print("post_files: {}\n".format(post_files))
+# print("all_files: {}\n".format(all_files))
 
 # Get list of unreferenced files
 unreferenced_files = all_files - post_files
@@ -36,6 +39,9 @@ unreferenced_files = all_files - post_files
 print('Total files:        {}'.format(len(all_files)))
 print('Post files:         {}'.format(len(post_files)))
 print('Unreferenced files: {}'.format(len(unreferenced_files)))
+
+if len(all_files) - len(post_files) != len(unreferenced_files):
+    raise Exception('Error! Too many files to delete! (all_files - post_files != unreferenced_files)')
 
 # print('Delete:')
 # for f in unreferenced_files:
